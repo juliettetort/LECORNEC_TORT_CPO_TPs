8fd03c7ed035e535ca39f80c7cc7b8d8bd0b5205
33782 <33782@192.168.115.131>
2021-10-08 17:26:39 +0200

    TP1_stats_TORT_LECORNEC

---
+++ TP1_stats_TORT_LECORNEC/src/tp1_stats_tort_lecornec/TP1_stats_TORT_LECORNEC.java
@@ -0,0 +1,31 @@
+/*
+ Statistique exo4
+ */
+package tp1_stats_tort_lecornec;
+import java.util.Scanner;
+/**
+ *
+ * @author 33782
+ */
+public class TP1_stats_TORT_LECORNEC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] tabentier =new int[6];
+        
+        for (int i=0; i<6;i++)
+            tabentier[i]=i;
+        System.out.println("rentrez un chiffre entre 1 et 6");
+        Scanner sc;
+        int m;
+        sc=new Scanner(System.in);
+        m=(int)sc.nextInt();
+        fot (int j=0,j<m)
+        
+            
+        
+    }
+    
+}
