65c67cee2b31cfd5fcc539716e4d13e1934a355f
33782 <33782@192.168.115.131>
2021-10-08 17:25:00 +0200

    manipNombresInt_TORT_LECORNEC

---
+++ manipNombresInt_TORT_LECORNEC/src/manipnombresint_tort_lecornec/Scanner.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manipnombresint_tort_lecornec;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author 33782
+ */
+class Scanner {
+
+    Scanner(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int nextInt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
