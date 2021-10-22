b67fdf7f53138f900e09e17ceb083e745ad9e995
33782 <33782@192.168.115.131>
2021-10-22 14:23:06 +0200

    TP2_Biere_TORT_LECORNEC

---
+++ TP2_Bieres_LECORNEC_TORT/src/tp2_bieres_lecornec_tort/TP2_Bieres_LECORNEC_TORT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_lecornec_tort;
+
+/**
+ *
+ * @author 33782
+ */
+public class TP2_Bieres_LECORNEC_TORT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
