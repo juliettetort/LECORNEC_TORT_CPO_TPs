e520ef881e657ef88fb9f56d3b83d09fff68d97f
guillaumelecornec <guillaumelecornec@192.168.115.68>
2021-11-12 18:28:26 +0100

    class cellule avec toutes les methodes (toutes les versions de couleur)

---
+++ SP4_console_TORT_LECORNEC/src/sp4_console_tort_lecornec/Cellule.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp4_console_tort_lecornec;
+
+/**
+ *
+ * @author guillaumelecornec
+ */
+public class Cellule {
+    Jeton jetonCourant;
+    boolean trouNoir;
+    boolean desintegrateur;
+
+    public void Cellule() {
+    jetonCourant=null;
+    trouNoir=true;
+    desintegrateur=true;
+}
+    public boolean affecterJeton(Jeton jeton1) {
+        if (jetonCourant==null) {
+            jetonCourant=jeton1;
+            return true;
+        }
+        else {
+                return false;
+                }
+        }
+     
+    public String recupererJeton() {
+        //a faire + tard 
+    }
+    public boolean supprimerjeton (){
+        if (jetonCourant==null) {
+            return false;
+        }
+        else {
+            return true;
+        }
+        }
+    public boolean placerTrouNoir() {
+        // chosir une case ou le trou noir va etre placé et le placer a cet endroit quoi qu il arrive , cette case prend une valeur "trounoir" 
+        if (jetonCourant==null){   // on verifie si la case choisie est vide ou pas 
+            return true;
+        }
+        else {
+            return false;
+        
+        }
+        }
+        
+    public boolean placerDesintegrateur() {
+        // chosir une case ou le desintegrateur va etre placé et le placer a cet endroit quoi qu il arrive , cette case prend une valeur "desintegrateur"
+        if (jetonCourant==null) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
+    public boolean presenceTrouNoir() {
+        if (jetonCourant==trounoir){       
+            return true;
+        }
+        else {
+            return false; 
+        }
+    }
+    public boolean presenceDesintegrateur() {
+        if (jetonCourant==desintegrateur) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
+    public String lireCouleurDuJeton() {
+        if (jetonCourant==null){
+            return "vide";
+        }
+        else {
+            return jetonCourant.Couleur;
+                    }
+        }
+    public boolean recupererDesintegrateur() {
+        if (jetonCourant==desintegrateur) {
+            //supprimer le jeton de la cellule en question
+            return true;
+        }
+        else {
+            return false;
+        }        
+    }
+    
+    public boolean activerTrouNoir() {
+        if (jetonCourant==trounoir) {
+            // engloutir le jeton
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
+    }
+    
+
+    
+
+
---
+++ SP4_console_TORT_LECORNEC/src/sp4_console_tort_lecornec/Jeton.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp4_console_tort_lecornec;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author guillaumelecornec
+ */
+public class Jeton {
+String Couleur;
+public void Jeton (String color) {
+    Couleur=color;
+}
+public Scanner lireCouleur() {
+    Scanner sc=new Scanner(System.in);
+    return sc;
+}
+}
