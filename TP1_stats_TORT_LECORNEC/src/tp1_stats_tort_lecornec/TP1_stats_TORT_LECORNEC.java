58976b9d58039c91a950341adffd7c7ecfd08ede
33782 <33782@192.168.115.131>
2021-11-12 16:00:02 +0100

    TP2_relation_1_TORT_LECORNEC_versionfinale

--- TP1_stats_TORT_LECORNEC/src/tp1_stats_tort_lecornec/TP1_stats_TORT_LECORNEC.java
+++ TP1_stats_TORT_LECORNEC/src/tp1_stats_tort_lecornec/TP1_stats_TORT_LECORNEC.java
@@ -2,6 +2,8 @@
  Statistique exo4
  */
 package tp1_stats_tort_lecornec;
+
+import java.util.Random;
 import java.util.Scanner;
 /**
  *
@@ -16,13 +18,18 @@ public class TP1_stats_TORT_LECORNEC {
         int [] tabentier =new int[6];
         
         for (int i=0; i<6;i++)
-            tabentier[i]=i;
+            tabentier[i]=0;
         System.out.println("rentrez un chiffre entre 1 et 6");
         Scanner sc;
         int m;
+        int generateurAleat;
         sc=new Scanner(System.in);
         m=(int)sc.nextInt();
-        fot (int j=0,j<m)
+        for (int j=0;j<=m; j++)
+                
+           
+            
+        
         
             
         
