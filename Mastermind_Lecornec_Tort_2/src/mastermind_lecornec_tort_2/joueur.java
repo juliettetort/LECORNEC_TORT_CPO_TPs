103dcef92a11b0fb4ab3f0b06927560f217d0f40
33782 <33782@192.168.115.131>
2021-12-03 17:09:57 +0100

    début Mastermind version 2 (3/12/21) fin du cours creation class joueur grille cellule et jetons

---
+++ Mastermind_Lecornec_Tort_2/src/mastermind_lecornec_tort_2/joueur.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mastermind_lecornec_tort_2;
+
+/**
+ *
+ * @author 33782
+ */
+public class joueur {
+    
+    public class Joueur {
+    String Nom;
+    String Couleur; 
+    jetons ListeJetons [] = new jetons [48];
+    
+}
+}
